8b7a0c44a9c8a10a16eea7f2aeabd96752d510d7
Saratyrn <saratyrnn@gmail.com>
2022-01-29 20:57:21 -0500

    first commit

---
+++ test/calculator.js
@@ -0,0 +1,29 @@
+
+function sum (a,b){
+ let a =3;
+ let b =2;   
+ let result= a+b;
+ return result;
+
+}
+function div (a,b){
+    let a =3;
+    let b =2;   
+    let result= a/b;
+    return result;
+   
+   }
+   function mul (a,b){
+    let a =3;
+    let b =2;   
+    let result= a*b;
+    return result;
+   
+   }
+   function sum (a,b){
+    let a =3;
+    let b =2;   
+    let result= a+b;
+    return result;
+   
+   }
